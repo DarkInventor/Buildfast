bf0e96d5309f05baa621c3a1b52f971d20ffeb6f
DarkInventor <ktmehta25@gmail.com>
2023-11-22 12:56:34 +0530

    trying to solve login error

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ load_dotenv()
 # Configuration
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_DISCOVERY_URL = os.getenv("GOOGLE_DISCOVERY_URL")
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
 # Flask app setup
 app = Flask(__name__)
